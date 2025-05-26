89f26b41c6746ff9b1c0df2773f654f2e3b7a12b
Leif <leifstoehr333@gmail.com>
2025-05-27 00:02:45 +0200

    CrossOrgigin

--- src/main/java/com/example/demo/RecipeController.java
+++ src/main/java/com/example/demo/RecipeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*") // ← das fehlt noch!
 @RestController
 public class RecipeController {
 
